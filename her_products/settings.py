b3083c96d8e2ba24c9a5bb3430a89398ec0215a6
Aishwarya-Varma123 <aishwaryavarma26@gmail.com>
2025-05-08 13:16:22 +0530

    Prepare for Render deploy

--- her_products/settings.py
+++ her_products/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,6 +37,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+ALLOWED_HOSTS = ['*']  # For testing, you can restrict later
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
